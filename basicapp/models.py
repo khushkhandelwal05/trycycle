41a5beaefdf399fa332e652705fe971323123353
khushkhandelwal05 <khushkhandelwal05@gmail.com>
2020-05-16 15:41:38 +0530

    My very first commit

---
+++ basicapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class userinfo(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    reg_no = models.CharField(max_length=100)
+    room_no = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone_no = models.BigIntegerField()
